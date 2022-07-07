41bc1bc661540a79344dd203e67571563344e842
Minwoo Choi <minwoo.choi@tapjoy.com>
2022-07-07 17:55:37 +0900

    feat: initial settings

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  const documentOptions: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, documentOptions);
+  const options = { customSiteTitle: 'Sample API Documentation' };
+  fs.writeFileSync('./openapi.json', JSON.stringify(document));
+  SwaggerModule.setup('api', app, document, options);
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(); // Allowed hosts might be restricted in production
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
