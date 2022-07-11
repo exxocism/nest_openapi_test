fd5dc067737cb0229c4406a18aa5234fe04a7937
Minwoo Choi <minwoo.choi@tapjoy.com>
2022-07-11 17:14:04 +0900

    feat: yaml creation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as yaml from 'yaml';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -20,6 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, documentOptions);
   const options = { customSiteTitle: 'Sample API Documentation' };
   fs.writeFileSync('./openapi.json', JSON.stringify(document));
+  fs.writeFileSync('./openapi.yaml', yaml.stringify(document, {}));
   fs.writeFileSync('postman/schemas/openapi_schema.json.json', JSON.stringify(document));
   SwaggerModule.setup('api', app, document, options);
 
