32d8b545cc6d0160689d3c20b4a4eb1ffcc151d0
Minwoo Choi <minwoo.choi@tapjoy.com>
2022-07-13 13:07:00 +0900

    feat: postman sync

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   };
   const document = SwaggerModule.createDocument(app, config, documentOptions);
   const options = { customSiteTitle: 'Sample API Documentation' };
-  fs.writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
+  fs.writeFileSync('./postman/schemas/openapi.json', JSON.stringify(document, null, 2));
   // fs.writeFileSync('./openapi.yaml', yaml.stringify(document, {}));
   SwaggerModule.setup('api', app, document, options);
 
